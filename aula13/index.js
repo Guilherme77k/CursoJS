fad99977c10bbf2b829b1b4facf8d43693da27b5
Guilherme Henrique Duarte <guilhermehenriquedds@gmail.com>
2024-06-12 17:36:58 -0300

    Aula13.

--- aula13/index.js
+++ aula13/index.js
@@ -1,3 +1,15 @@
 let umaString = 'Um texto';
 
-console.log(umaString);
+console.log(`${umaString} em um lindo dia.`);
+
+console.log(umaString.lastIndexOf('m', 3));
+
+console.log(umaString.search(/[x]/));
+
+console.log(umaString.replace('Um', 'Outra'));
+
+let umaString2 ='O rato roeu a roupa do rei de roma.';
+
+console.log(umaString2.replace(/r/g, '#'));
+
+console.log(umaString2.length); 
