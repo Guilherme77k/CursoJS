fea350ea73eb15b4fb8ff9d3a495051b97cd5f72
Guilherme Henrique Duarte <guilhermehenriquedds@gmail.com>
2024-06-13 11:18:28 -0300

    Aula15.

---
+++ aula14/index.js
@@ -0,0 +1,12 @@
+let num1 = 100; //number
+let num2 = 2.5 //number
+
+// console.log (num1.toString() + num2);
+// console.log (typeof num1);
+// console.log(num1.toString(2));
+// console.log(num1.toFixed(2));
+// console.log(Number.isInteger(num1));
+// let temp = num1 + '6';
+// console.log(Number.isNaN(temp));
+
+console.log(temp)
---
+++ aula15/index.js
@@ -0,0 +1,11 @@
+let num1 = 9.55555
+let num2 = Math.floor(num1); //arredonda pra baixo
+let num3 = Math.ceil(num1); // arredonda pra cima
+let num4 = Math.round(num1); // arredonda de acordo com a matematica simples
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+console.log(Math.random() * (10 - 5) + 5);
