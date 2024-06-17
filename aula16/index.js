16ccf37b9c1d681de5ac38f8cec2414f3e63cea9
Guilherme Henrique Duarte <guilhermehenriquedds@gmail.com>
2024-06-17 13:27:00 -0300

    Aula16.

---
+++ aula16/index.js
@@ -0,0 +1,10 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+// alunos.unshift('Luiza') // Adiciona no começo
+// alunos.unshift('Fabio') // Adiciona no fim
+// alunos.pop(); Remove no fim
+// alunos.shift(); Remove o primeiro
+// delete alunos[1];
+// console.log(alunos.slice(0, -2));
+
+console.log(alunos);
